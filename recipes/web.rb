9ed371cd8936ce4f9791706aab8b1c3e452ec6d1
amiando <administrator@amiando.com>
2012-07-25 21:06:36 +0300

    Update recipes/web.rb
    Fix for: FATAL: Chef::Mixin::Template::TemplateError: undefined method `each' for "zabbix":String  Change to array and set default in attributes file.

--- recipes/web.rb
+++ recipes/web.rb
@@ -64,7 +64,7 @@ end
 #install vhost for zabbix frontend
 web_app "#{node.zabbix.web.fqdn}" do
   server_name "#{node.zabbix.web.fqdn}"
-  server_aliases [ "#{node.zabbix.web.alias}" ]
+  server_aliases node.zabbix.web.aliases
   docroot "#{node.zabbix.web_dir}"
   only_if { "#{node.zabbix.web.fqdn}" != nil }
 end  
