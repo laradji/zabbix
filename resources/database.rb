a027dc64967aa5125e4b5a4a40789ba21403b1f8
Andrew Garson <andrew.garson@gmail.com>
2013-04-04 13:45:45 -0700

    Set default provider for resources/database in such a way that we can name provider files well

--- resources/database.rb
+++ resources/database.rb
@@ -1,6 +1,11 @@
 actions :create
 default_action :create
 
+def initialize(name, run_context=nil)
+  super
+  @provider ||= Chef::Provider::ZabbixDatabaseMySql
+end
+
 attribute :dbname, :kind_of => String, :name_attribute => true
 attribute :host, :kind_of => String, :required => true
 attribute :username, :kind_of => String, :required => true
