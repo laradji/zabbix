37ebe66b2a708d853f077c33764a33c1fd086b04
Andrew Garson <andrew.garson@gmail.com>
2013-04-12 13:22:19 -0700

    Pass php settings into nginx and apache zabbix config templates

--- attributes/default.rb
+++ attributes/default.rb
@@ -35,7 +35,14 @@ default['zabbix']['web']['install_method']  = 'apache'
 default['zabbix']['web']['fqdn']            = nil
 default['zabbix']['web']['aliases']         = ["zabbix"]
 default['zabbix']['web']['port']            = 80
-default['zabbix']['web']['timezone']        = "UTC"
+default['zabbix']['web']['php_settings']    = {
+  "memory_limit"        => "256M",
+  "post_max_size"       => "32M",
+  "upload_max_filesize" => "16M",
+  "max_execution_time"  => "600",
+  "max_input_time"      => "600",
+  "date.timezone"       => "'UTC'",
+}
 
 default['zabbix']['install_dir']  = "/opt/zabbix"
 default['zabbix']['etc_dir']      = "/etc/zabbix"
