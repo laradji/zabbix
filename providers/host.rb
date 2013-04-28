4f369ab82493e7b0763a866495e3fb1e228ba8c3
Andrew Garson <andrew.garson@gmail.com>
2013-04-28 13:20:09 -0700

    Adds simple host registration LWRP
    Fixes type in providers/host_group.rb

---
+++ providers/host.rb
@@ -0,0 +1,35 @@
+action :create_or_update do
+
+  chef_gem "zabbixapi" do
+    action :install
+    version "~> 0.5.9"
+  end
+
+  require 'zabbixapi'
+
+  Chef::Zabbix.with_connection(new_resource.server_connection) do |connection|
+    group_ids = new_resource.groups.map do |group|
+      connection.hostgroups.get_id(:name => group) ||
+        connection.hostgroups.create(:host => group)
+    end
+
+    interfaces = [
+      {
+        :type => 1,
+        :main => 1,
+        :ip => '',
+        :port => 10050,
+        :dns => new_resource.hostname,
+        :useip => 0
+      }
+    ]
+
+    connection.hosts.create_or_update(
+      :host => new_resource.hostname,
+      :interfaces => interfaces,
+      :groups => group_ids
+    )
+  end
+
+  updated_by_last_action(true)
+end
