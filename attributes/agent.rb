6c8b05570a6d35689f55e0af783b9592d1401430
Guilhem Lettron <guilhem@lettron.fr>
2013-05-17 12:08:29 +0200

    New configuration file for agent Linux/Zabbix log to syslog by default use default start_agent do not set pid_fil for windows enable remote_command by default but can be switch off for security

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -10,17 +10,21 @@ default['zabbix']['agent']['servers_active']    = []
 default['zabbix']['agent']['hostname']          = node['fqdn']
 default['zabbix']['agent']['configure_options'] = [ "--with-libcurl" ]
 default['zabbix']['agent']['include_dir']       = ::File.join( node['zabbix']['etc_dir'] , "agent_include")
+default['zabbix']['agent']['enable_remote_commands'] = true
 
 default['zabbix']['agent']['groups']            = [ "chef-agent" ]
-
-default['zabbix']['agent']['prebuild']['arch']  = node['kernel']['machine'] == "x86_64" ? "amd64" : "i386"
-default['zabbix']['agent']['prebuild']['url']      = "http://www.zabbix.com/downloads/#{node['zabbix']['agent']['version']}/zabbix_agents_#{node['zabbix']['agent']['version']}.linux2_6.#{node['zabbix']['agent']['prebuild']['arch']}.tar.gz"
 
 case node['platform_family']
 when "rhel", "debian"
   default['zabbix']['agent']['init_style']      = "sysvinit"
   default['zabbix']['agent']['install_method']  = "prebuild"
+  default['zabbix']['agent']['pid_file']         = ::File.join(node['zabbix']['run_dir'], "zabbix_agentd.pid")
 when "windows"
   default['zabbix']['agent']['init_style']      = "windows"
   default['zabbix']['agent']['install_method']  = "chocolatey"
 end
+
+default['zabbix']['agent']['log_file']           = nil # default (Syslog / windows event).
+#default['zabbix']['agent']['log_file']           = ::File.join(node['zabbix']['log_dir'], "zabbix_agentd.log"
+default['zabbix']['agent']['start_agents']       = nil # default (3)
+default['zabbix']['agent']['debug_level']        = nil # default (3)
