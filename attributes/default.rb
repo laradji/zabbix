a38b7f77abcd57846aa41b0073a748400a3b1b5b
Guilhem Lettron <guilhem@lettron.fr>
2013-05-17 11:10:59 +0200

    Fix path in wow64 env

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,11 @@
 
 case node['platform_family']
 when "windows"
-  default['zabbix']['etc_dir']    = File.join(ENV['ProgramFiles'], "Zabbix Agent")
+  if ENV['ProgramFiles'] == ENV['ProgramFiles(x86)']
+    default['zabbix']['etc_dir']    = File.join(ENV['homedrive'], "Program Files", "Zabbix Agent")
+  else
+    default['zabbix']['etc_dir']    = File.join(ENV['ProgramFiles'], "Zabbix Agent")
+  end
 else
   default['zabbix']['etc_dir']    = "/etc/zabbix"
 end
