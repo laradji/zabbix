12412e1e9bfc26680e1373e7163ed5e86441b8a5
Andrew Garson <andrew.garson@gmail.com>
2013-05-22 19:10:11 -0700

    Fix zabbix_template :create

--- resources/application.rb
+++ resources/application.rb
@@ -1,5 +1,7 @@
 actions :create
 default_action :create
 
+attribute :name, :kind_of => String, :name_attribute => true
 attribute :server_connection, :kind_of => Hash, :required => true
+attribute :group, :kind_of => String, :default => "Templates"
 attribute :parameters, :kind_of => Hash, :required => true
--- resources/template.rb
+++ resources/template.rb
@@ -1,5 +1,6 @@
 actions :create, :delete
 default_action :create
 
+attribute :name, :kind_of => String, :required => true
 attribute :server_connection, :kind_of => Hash, :required => true
-attribute :parameters, :kind_of => Hash, :required => true
+attribute :group, :kind_of => String, :default => "Templates"
