7db651035d22b1e98da5b2c8787d23263db67445
Andrew Garson <andrew.garson@gmail.com>
2013-06-18 01:38:11 -0700

    make delay have a default instead of being required

--- resources/item.rb
+++ resources/item.rb
@@ -8,10 +8,10 @@ attribute :template, :kind_of => String, :required => true
 attribute :applications, :kind_of => Array, :required => true
 attribute :key, :kind_of => String, :required => true
 attribute :type, :kind_of => Chef::Zabbix::API::ItemType, :required => true
-attribute :delay, :kind_of => Fixnum, :required => true
 attribute :value_type, :kind_of => Chef::Zabbix::API::ItemValueType, :required => true 
 attribute :server_connection, :kind_of => Hash, :required => true
 
+attribute :delay, :kind_of => Fixnum, :default => 60
 attribute :description, :kind_of => String
 attribute :snmp_community, :kind_of => String, :default => '{}'
 attribute :snmp_oid, :kind_of => String, :default => '{}'
