02203c42d29d075873a2c19503d9ebca3bd5620e
Yann <yann.robin@youscribe.com>
2013-08-28 14:46:02 +0200

    fix error on windows when creating root_dirs
    fix error on windows when creating root_dirs. On windows, user should be
    Administrator.

--- recipes/common.rb
+++ recipes/common.rb
@@ -13,12 +13,23 @@ root_dirs = [
 ]
 
 # Create root folders
-root_dirs.each do |dir|
-  directory dir do
-    owner "root"
-    group "root"
-    mode "755"
-    recursive true
+case node['platform_family']
+when "windows"
+  root_dirs.each do |dir|
+    directory dir do
+      owner "Administrator"
+      rights :read, "Everyone", option  => :applies_to_children
+      recursive true
+    end
+  end
+else
+  root_dirs.each do |dir|
+    directory dir do
+      owner "root"
+      group "root"
+      mode "755"
+      recursive true
+    end
   end
 end
 
