a9db783658bb9c6c3ff1ab651b6b13fc4cb10753
Nacer Laradji <nacer.laradji@fiduceo.fr>
2013-12-16 12:48:21 +0100

    We need to have the user created before running common

--- recipes/agent_common.rb
+++ recipes/agent_common.rb
@@ -1,4 +1,4 @@
-include_recipe "zabbix::common"
 include_recipe "zabbix::_agent_common_user"
+include_recipe "zabbix::common"
 include_recipe "zabbix::_agent_common_directories"
 include_recipe "zabbix::_agent_common_service"
