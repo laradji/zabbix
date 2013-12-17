f32825a31dd23555acd65a36bc7ffec71d733fc6
Ian Delahorne <ian.delahorne@gmail.com>
2013-12-17 13:53:43 +0100

    Added zabbix::_repository for Zabbix official packages
    Supports both apt and yum. Set node['zabbix']['major_version'] for the major
    version of Zabbix, default is 2.0.

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,3 +28,5 @@ default['zabbix']['uid']    = nil
 default['zabbix']['gid']    = nil
 default['zabbix']['home']   = '/opt/zabbix'
 default['zabbix']['shell']  = "/bin/bash"
+
+default['zabbix']['major_version'] = '2.0'
