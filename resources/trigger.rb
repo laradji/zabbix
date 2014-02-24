be8ffa62deceb50d1cf303577f83fe2a7356a3f1
Kevin <kevin@kbrwadventure.com>
2014-02-24 18:21:24 +0100

    add attribut template

--- resources/trigger.rb
+++ resources/trigger.rb
@@ -1,10 +1,11 @@
-actions :create
+actions :create, :delete
 default_action :create
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :expression, :kind_of => String, :required => true
 attribute :priority, :kind_of => Zabbix::API::TriggerPriority, :required => true
 attribute :description, :kind_of => String
+attribute :template, :kind_of => String
 attribute :status, :kind_of => Zabbix::API::TriggerStatus, :default => Zabbix::API::TriggerStatus.active
 attribute :type, :kind_of => Zabbix::API::TriggerType, :default => Zabbix::API::TriggerType.normal
 
