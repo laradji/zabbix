574f4fe04e9f64e3d9e1c9c8e182c13bff16a6c0
Kevin <kevin@kbrwadventure.com>
2014-02-24 18:14:33 +0100

    application addded for host and not only for template. Goal is to be able to add a application to a host without using template, sometimes usefull

--- providers/application.rb
+++ providers/application.rb
@@ -2,19 +2,31 @@ action :create do
   Chef::Zabbix.with_connection(new_resource.server_connection) do |connection|
     # Convert the "hostname" (a template name) into a hostid
 
-    template_ids = Zabbix::API.find_template_ids(connection, new_resource.template)
-    if template_ids.empty?
-      Chef::Application.fatal! "Could not find a template named #{new_resource.template}"
+    template_id=""
+    if new_resource.template
+      template_ids = Zabbix::API.find_template_ids(connection, new_resource.template)
+      if template_ids.empty?
+        Chef::Application.fatal! "Could not find a template named #{new_resource.template}"
+      end
+      template_id = template_ids.first['templateid']
+    end
+    if new_resource.hostname
+      template_ids = Zabbix::API.find_host_ids(connection, new_resource.hostname)
+      if template_ids.empty?
+        Chef::Application.fatal! "Could not find a host named #{new_resource.hostname}"
+      end
+      template_id = template_ids.first['hostid']
     end
 
-    application_ids = Zabbix::API.find_application_ids(connection, new_resource.name, template_ids.first)
+    #application_ids = Zabbix::API.find_application_ids(connection, new_resource.name, template_ids.first)
+    application_ids = Zabbix::API.find_application_ids(connection, new_resource.name, template_id)
 
     if application_ids.empty?
       request = {
         :method => "application.create",
         :params => {
           :name => new_resource.name,
-          :hostid => template_ids.first['hostid']
+          :hostid => template_id
         }
       }
       connection.query(request)
