3ae779526d833c3ea6e391408cf913888fb9d077
Kevin <kevin@kbrwadventure.com>
2014-02-24 18:19:08 +0100

    add attribute hostname and set attribute template as no required. In fact hostname or template must be defined

--- resources/application.rb
+++ resources/application.rb
@@ -3,4 +3,5 @@ default_action :create
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :server_connection, :kind_of => Hash, :required => true
-attribute :template, :kind_of => String, :required => true
+attribute :template, :kind_of => String
+attribute :hostname, :kind_of => String
