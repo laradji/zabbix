ec435fb34fc0faccc01654fb40ed47b598be29c6
Yusuke Goto <u.suke.goto@gmail.com>
2014-04-10 01:27:43 +0900

    Pass the rubocop

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,17 +1,17 @@
-include_attribute "zabbix"
+include_attribute 'zabbix'
 
-default['zabbix']['server']['version']                = "2.0.3"
-default['zabbix']['server']['branch']                 = "ZABBIX%20Latest%20Stable"
+default['zabbix']['server']['version']                = '2.0.3'
+default['zabbix']['server']['branch']                 = 'ZABBIX%20Latest%20Stable'
 default['zabbix']['server']['source_url']             = nil
-default['zabbix']['server']['install_method']         = "source"
-default['zabbix']['server']['configure_options']      = [ "--with-libcurl","--with-net-snmp"]
-default['zabbix']['server']['include_dir']            = "/opt/zabbix/server_include"
-default['zabbix']['server']['log_file']               = ::File.join(node['zabbix']['log_dir'], "zabbix_server.log")
+default['zabbix']['server']['install_method']         = 'source'
+default['zabbix']['server']['configure_options']      = ['--with-libcurl', '--with-net-snmp']
+default['zabbix']['server']['include_dir']            = '/opt/zabbix/server_include'
+default['zabbix']['server']['log_file']               = ::File.join(node['zabbix']['log_dir'], 'zabbix_server.log')
 default['zabbix']['server']['log_level']              = 3
-default['zabbix']['server']['housekeeping_frequency'] = "1"
-default['zabbix']['server']['max_housekeeper_delete'] = "100000"
+default['zabbix']['server']['housekeeping_frequency'] = '1'
+default['zabbix']['server']['max_housekeeper_delete'] = '100000'
 
-default['zabbix']['server']['host'] = "localhost"
+default['zabbix']['server']['host'] = 'localhost'
 default['zabbix']['server']['port'] = 10051
 default['zabbix']['server']['name'] = nil
 
@@ -20,7 +20,7 @@ default['zabbix']['server']['java_gateway_port'] = 10052
 default['zabbix']['server']['java_pollers'] = 0
 default['zabbix']['server']['start_pollers'] = 5
 
-default['zabbix']['server']['externalscriptspath'] = "/usr/local/scripts/zabbix/externalscripts/"
+default['zabbix']['server']['externalscriptspath'] = '/usr/local/scripts/zabbix/externalscripts/'
 
-default['zabbix']['server']['timeout'] = "3"
-default['zabbix']['server']['value_cache_size'] = "8M" # default 8MB
+default['zabbix']['server']['timeout'] = '3'
+default['zabbix']['server']['value_cache_size'] = '8M' # default 8MB
