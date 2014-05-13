46f2a4c7b773479a237c3b17a1ccff8b39cceb7c
Tilman Holschuh <tholschuh@pugpharm.com>
2014-05-13 23:57:09 +0000

    add chefspec test for agentd_prebuild recipe

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
 require 'chefspec'
+require 'chefspec/berkshelf'
+
+def put_ark(resource_name)
+  ChefSpec::Matchers::ResourceMatcher.new(:ark, :put, resource_name)
+end
+
+at_exit { ChefSpec::Coverage.report! }
