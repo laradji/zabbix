71fce991b79c8ec0e4b86871a0f82422b91d1c27
Jorge Espada <jorge.espada@wwwh.com>
2014-06-26 11:54:34 -0300

    fix typo

--- recipes/agent_package_custom.rb
+++ recipes/agent_package_custom.rb
@@ -8,7 +8,7 @@
 #
 
 #in case you using a wrapper or already have a recipe to install custom repos
-include_recipe node['zabbix']['agent_package_custom']['custom_repo_recipe'] if nodet['zabbix']['agent_package_custom']['custom_repo_recipe']
+include_recipe node['zabbix']['agent_package_custom']['custom_repo_recipe'] if node['zabbix']['agent_package_custom']['custom_repo_recipe']
 
 package 'zabbix-agent' do
   package_name node['zabbix']['agent_package_custom']['package_name']
