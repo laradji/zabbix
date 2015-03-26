7e5ce7e30f2d2d61e4bd018592680c26b8dcbc3a
Javier Bértoli <javier@netmanagers.com.ar>
2015-03-26 13:49:47 -0300

    Remove prerequisite that breaks centos6 tests

--- recipes/agent_package_custom.rb
+++ recipes/agent_package_custom.rb
@@ -10,9 +10,6 @@
 # in case you using a wrapper or already have a recipe to install custom repos
 include_recipe node['zabbix']['agent_package_custom']['custom_repo_recipe'] if node['zabbix']['agent_package_custom']['custom_repo_recipe']
 
-# Install prerequisite RPM
-package 'redhat-lsb' if node['platform_family'] == 'rhel'
-
 package 'zabbix-agent' do
   package_name node['zabbix']['agent_package_custom']['package_name']
   version node['zabbix']['agent_package_custom']['package_version']
