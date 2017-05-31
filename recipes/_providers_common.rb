2d5d5481d90177432e7ec1c41dcf34907587145a
James Le Cuirot <james.le-cuirot@yakara.com>
2017-05-31 14:23:32 +0100

    Determine the zabbixapi gem version automatically
    This doesn't change the fact that zabbixapi versions older than 3.0.0
    do not work against Chef versions older than 12.15.9 due to the json
    gem version conflicting.
    
    I have pushed a revised version of zabbixapi 2.2.2 that allows json
    2.x to my packagecloud repository. You are free to use this and I can
    create other versions on request.
    
    https://packagecloud.io/chewi/zabbix

--- recipes/_providers_common.rb
+++ recipes/_providers_common.rb
@@ -1,4 +1,13 @@
 chef_gem 'zabbixapi' do
   action :install
-  version '~> 0.6.3'
+  server_version = node['zabbix']['server']['version'].scan(/^\d+\.\d+/).first
+
+  case server_version
+  when '1.8'
+    version '~> 0.6.3'
+  when '2.0', '2.2', '2.4', '3.0'
+    version "~> #{server_version}.0"
+  when '3.2'
+    version '~> 3.0.0'
+  end
 end
